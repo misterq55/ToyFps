1d66f17a5562a489b1a6b473c7e1da11bf849f22
규 서문 <illsi@naver.com>
2023-02-07 18:25:08 +0900

    작업중

--- Source/ToyFPS/ToyFpsCharacter.cpp
+++ Source/ToyFPS/ToyFpsCharacter.cpp
@@ -59,6 +59,14 @@ void AToyFpsCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	//Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 }
 
 // Called every frame
